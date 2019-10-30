0d769f59a5e1b13e38e2366faf3de00aabe54fe7
CinderyCape27 <julio_denis@icloud.com>
2019-10-30 11:31:31 -0600

    front completed

--- src/components/CreateNotes.js
+++ src/components/CreateNotes.js
@@ -5,36 +5,49 @@ import 'react-datepicker/dist/react-datepicker.css'
 
 export default class CreateNotes extends Component {
     state = {
-        users: [],
-        userSelected: '',
         title:'',
         content: '',
-        date: new Date()
+        date: new Date(),
+        userSelected: '',
+        users: [],
+        editing: false,
+        _id: ''
     }
 
     async componentDidMount() {
         const res = await axios.get('http://localhost:4000/api/users')
         this.setState({ users: res.data.map(user => user.username),
             userSelected: res.data[0].username
-         })
-        
+        })
+        if(this.props.match.params.id) {
+            const res = await axios.get('http://localhost:4000/api/notes/' + this.props.match.params.id);
+            this.setState({
+                title: res.data.title,
+                content: res.data.content,
+                date: new Date(res.data.date),
+                userSelected: res.data.author,
+                editing: true,
+                _id: this.props.match.params.id
+            })
+        }
     }
 
 
 
 
     onSubmit = async (e) => {
-        console.log(this.state.title, this.state.content)
         e.preventDefault();
-
         const newNote = {
-            
             title: this.state.title,
             content: this.state.content,
             date: this.state.date,
             author: this.state.userSelected
         };
-        await axios.post('http://localhost:4000/api/notes', newNote)
+        if (this.state.editing) {
+            await axios.put('http://localhost:4000/api/notes/' + this.state._id, newNote);
+        } else {
+            await axios.post('http://localhost:4000/api/notes', newNote);
+        }
         window.location.href = '/';
 
     }
@@ -48,7 +61,7 @@ export default class CreateNotes extends Component {
     }
     render() {
 
-        return ( 
+        return (
             <div className="col-md-6 offset-md-3">
                 <div className="card card-body">
                     <h4>Create a Note</h4>
@@ -68,27 +81,33 @@ export default class CreateNotes extends Component {
                             }
                         </select>
                     </div>
+
                     <div className="form-group">
                         <input
                             type="text"
                             className="form-control"
                             placeholder="Title"
-                            name="title"
                             onChange={this.onInputChange}
+                            name="title"
+                            value={this.state.title}
                             required
                         />
                     </div>
 
                     <div className="form-group">
                         <textarea
+                            type="text"
                             name="content"
                             className="form-control"
                             placeholder="Content"
                             onChange={this.onInputChange}
+                            value={this.state.content}
                             required
                         >
                         </textarea>
+
                     </div>
+
                     <div className="form-group">
                         <DatePicker
                             className="form-control"
--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 import { format } from 'timeago.js';
+import { Link } from 'react-router-dom';
 
 export default class NotesList extends Component {
 
-    state =  {
+    state = {
         notes: []
     }
 
@@ -14,7 +15,7 @@ export default class NotesList extends Component {
 
     async getNotes() {
         const res = await axios.get('http://localhost:4000/api/notes')
-        this.setState({notes: res.data})
+        this.setState({ notes: res.data })
     }
 
     deleteNote = async (id) => {
@@ -29,8 +30,11 @@ export default class NotesList extends Component {
                     this.state.notes.map(note => (
                         <div className="col-md-4 p-2" key={note._id}>
                             <div className="card">
-                                <div className="card-header">
+                                <div className="card-header d-flex justify-content-between">
                                     <h5>{note.title}</h5>
+                                    <Link className="btn btn-secondary" to={"/edit/" + note._id}>
+                                        Edit
+                                    </Link>
                                 </div>
                                 <div className="card-body">
                                     <p> <strong> Description:</strong> {note.content}</p>
@@ -38,7 +42,7 @@ export default class NotesList extends Component {
                                     <p>{format(note.date)}</p>
                                 </div>
                                 <div className="card-footer">
-                                    <button className="btn btn-danger" onClick={() => {this.deleteNote(note._id)}}>
+                                    <button className="btn btn-danger" onClick={() => { this.deleteNote(note._id) }}>
                                         Delete
                                     </button>
                                 </div>
